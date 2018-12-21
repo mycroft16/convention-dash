fff60ecf6e3589d446897dea7af580ac07e62189
Adam Morgan <acm1979@gmail.com>
2018-12-21 03:36:31 -0700

    app component updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
+  selector: 'fanx-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'fanx-dash';
+
+  public hideElement$: Observable<boolean> = this.router.events
+    .pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map((data: any) => (data.url === '/login' || data.url === '' || data.url === '/'))
+    );
+
+  constructor(private router: Router) { }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppComponent } from './app.component';
+import { COMPONENTS } from './common/components';
+import { GUARDS } from './common/guards';
 import { MODULES } from './common/modules';
+
+
+// RXJS
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { STORES, EFFECTS, PROVIDERS, metaReducers } from './common/store';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ...COMPONENTS
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    StoreModule.forRoot(STORES, { metaReducers }),
+    StoreDevtoolsModule.instrument(),
+    EFFECTS,
     ...MODULES
   ],
-  providers: [],
+  providers: [
+    ...GUARDS,
+    ...PROVIDERS
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

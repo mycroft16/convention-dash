0e8869e5f885d6983c2ebd2f954450e96791c033
Adam Morgan <acm1979@gmail.com>
2018-12-22 13:09:06 -0700

    create components and routing for guests, settings, vendors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './common/guards/auth/auth.guard';
 
 import { DashboardComponent } from './common/components/dashboard/dashboard.component';
+import { GuestsComponent } from './common/components/guests/guests.component';
 import { LoginComponent } from './common/components/login/login.component';
+import { SettingsComponent } from './common/components/settings/settings.component';
+import { VendorsComponent } from './common/components/vendors/vendors.component';
 
 const routes: Routes = [
 
@@ -12,14 +15,17 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     component: DashboardComponent
   },
+  { path: 'guests', component: GuestsComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'vendors', component: VendorsComponent },
 
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'dashboard',
     pathMatch: 'full'
   },
-  { path: '**', redirectTo: '/login' }
+  { path: '**', redirectTo: 'dashboard' }
 ];
 
 @NgModule({
--- src/app/common/components/index.ts
+++ src/app/common/components/index.ts
@@ -1,11 +1,17 @@
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { GuestsComponent } from './guests/guests.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { SettingsComponent } from './settings/settings.component';
+import { VendorsComponent } from './vendors/vendors.component';
 
 export const COMPONENTS = [
     DashboardComponent,
+    GuestsComponent,
     HeaderComponent,
     LoginComponent,
-    NavigationComponent
+    NavigationComponent,
+    SettingsComponent,
+    VendorsComponent
 ]
